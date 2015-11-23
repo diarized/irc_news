9fcb7ff40ab93d6415d115ad7ec4637198c50eef
Artur Kaminski <my.home.my.castle@gmail.com>
2015-11-23 14:13:31 +0100

    API partially moved to flask_restful

--- src/api.py
+++ src/api.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
 
 from flask import Flask, request, jsonify
+import flask_restful as rest
 import storage
-import requests
 
 app = Flask(__name__)
+api = rest.Api(app)
 db = storage.LinksDB()
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+parser = rest.reqparse.RequestParser()
+parser.add_argument('link')
+parser.add_argument('title')
 
-@app.route('/api/v1.0/links', methods=['POST'])
-@app.route('/api/v1.0/links/<link_pattern>', methods=['GET'])
-def search_link(link_pattern=None):
-    if request.method == 'POST':
-        link_pattern = request.form['link']
-    rows = db.search_link(link_pattern)
-    return jsonify(rows)
+class HelloWorld(rest.Resource):
+    def get(this):
+        return {'hello': 'world!'}
+
+class LinksApi(rest.Resource):
+    def post(this):
+        args = parser.parse_args()
+        link_pattern = args['link']
+        rows = db.search_link(link_pattern)
+        return jsonify(rows)
+
+    def get(this, link_pattern):
+        rows = db.search_link(link_pattern)
+        return jsonify(rows)
+
 
 @app.route('/api/v1.0/titles', methods=['POST'])
 @app.route('/api/v1.0/titles/<title_pattern>', methods=['GET'])
@@ -38,4 +47,8 @@ def post_link():
 
 if __name__ == '__main__':
     # Test at http://cheap.stonith.pl:7777/api/v1.0/links/microsoft
+    # app.run(debug=True, host='0.0.0.0', port=7777)
+
+    api.add_resource(HelloWorld, '/')
+    api.add_resource(LinksApi, '/api/v1.0/links', '/api/v1.0/links/<link_pattern>')
     app.run(debug=True, host='0.0.0.0', port=7777)
