4765aad5cb593331173f91ea4a97e4ed2f77a15f
Artur Kaminski <my.home.my.castle@gmail.com>
2016-03-14 12:40:26 +0100

    Verbose plugins to debug responses

--- src/plugins.py
+++ src/plugins.py
@@ -11,13 +11,19 @@ def strip_tag(s):
     txt = ' '.join(txt)             # 'foo   bar'
     return re.sub('\s+', ' ', txt)  # 'foo bar'
 
+def help(*args, **kwargs):
+    output = ["Use google search_string to get example functionlity of the system"]
+    return output
 
 def google(searchfor=''):
+    if not len(searchfor):
+        return ['']
     link = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&{}'.format(searchfor)
     ua = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
     payload = {'q': searchfor}
     response = requests.get(link, headers=ua, params=payload)
     response_text = json.loads(response.text)
+    pprint.pprint(response_text)
     results = response_text['responseData']['results']
     output = []
     for result in results[:min(3, len(results))]:
